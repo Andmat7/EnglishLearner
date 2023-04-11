ed43d4ebb1ae5ff2a767cb173e0a7e22e8647339
Andres1~ <andrescorredor20@gmail.com>
2023-04-11 08:38:44 -0500

    enableVideoAndAudio

--- VideoRecorder.js
+++ VideoRecorder.js
@@ -2,21 +2,38 @@ import { HtmlElementsFactory } from './HtmlElementsFactory.js';
 export class VideoRecorder {
     constructor() {
         this.createHtmlElements();
-        this.initializeRecorder();
+        this.chunks = [];
+        this.recorder = null;
     }
 
     createHtmlElements() {
         const recordBtn = { tag: 'button', label: 'Record', onClick: this.startRecording.bind(this) };
         const stopBtn = { tag: 'button', label: 'Stop', onClick: this.stopRecording.bind(this), style: 'display: none;' };
         const video = { tag: 'video', attributes: { controls: true, muted: '' }, id: 'myVideo' };
-        const cameraBtn = { tag: 'button', label: 'Camera', onClick: this.toggleCamera.bind(this) };
+        const enableVideo = { tag: 'button', label: 'Enable video', onClick: this.toggleCamera.bind(this) };
 
         const videoRecorder = document.querySelector('video-recorder');
-        const createdElements = HtmlElementsFactory.appendTo(videoRecorder, [recordBtn, stopBtn, cameraBtn, video]);
+        const createdElements = HtmlElementsFactory.appendTo(videoRecorder, [recordBtn, stopBtn, enableVideo, video]);
 
         this.recordBtn = createdElements[0];
         this.stopBtn = createdElements[1];
         this.cameraBtn = createdElements[2];
+        this.videoElement = createdElements[3];
+    }
+
+    enableVideoAndAudio() {
+        const isVideoSupported = navigator.mediaDevices && navigator.mediaDevices.getUserMedia
+        if (isVideoSupported) {
+
+            navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(stream => {
+                this.startStreaming(stream);
+
+            }).catch(error => {
+                console.error('No se pudo acceder a la cámara y/o micrófono.', error);
+            });
+        } else {
+            console.error('La API de getUserMedia no está disponible en este navegador.');
+        }
     }
 
     toggleCamera() {
@@ -36,22 +53,7 @@ export class VideoRecorder {
         }
     }
 
-    initializeRecorder() {
-        this.chunks = [];
-        this.recorder = null;
-        this.videoElement = document.querySelector('video');
 
-        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-            navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(stream => {
-                this.startStreaming(stream);
-
-            }).catch(error => {
-                console.error('No se pudo acceder a la cámara y/o micrófono.', error);
-            });
-        } else {
-            console.error('La API de getUserMedia no está disponible en este navegador.');
-        }
-    }
 
     handleRecordingStop() {
         const videoData = new Blob(this.chunks, { type: 'video/webm' });
