6eebb82c4e8d7f50145d0f0405047bfdde9e4681
Andres1~ <andrescorredor20@gmail.com>
2023-04-11 09:36:22 -0500

    isVideoSupported

--- VideoRecorder.js
+++ VideoRecorder.js
@@ -7,14 +7,14 @@ export class VideoRecorder {
     }
 
     createHtmlElements() {
-        const recordBtn = { tag: 'button', label: 'Record', onClick: this.startRecording.bind(this), style: 'display: none;'  };
+        const recordBtn = { tag: 'button', label: 'Record', onClick: this.startRecording.bind(this), style: 'display: none;' };
         const stopBtn = { tag: 'button', label: 'Stop', onClick: this.stopRecording.bind(this), style: 'display: none;' };
         const video = { tag: 'video', attributes: { controls: true, muted: '' }, id: 'myVideo' };
         const enableVideoBtn = { tag: 'button', label: 'Enable video', onClick: this.enableVideo.bind(this) };
-        const disableVideoBtn = { tag: 'button', label: 'Disable video', onClick: this.disableVideo.bind(this), style: 'display: none;'  };
+        const disableVideoBtn = { tag: 'button', label: 'Disable video', onClick: this.disableVideo.bind(this), style: 'display: none;' };
 
         const videoRecorder = document.querySelector('video-recorder');
-        const createdElements = HtmlElementsFactory.appendTo(videoRecorder, [recordBtn, stopBtn, enableVideoBtn,disableVideoBtn, video]);
+        const createdElements = HtmlElementsFactory.appendTo(videoRecorder, [recordBtn, stopBtn, enableVideoBtn, disableVideoBtn, video]);
 
         this.recordBtn = createdElements[0];
         this.stopBtn = createdElements[1];
@@ -24,21 +24,27 @@ export class VideoRecorder {
     }
 
     enableVideo() {
-        const isVideoSupported = navigator.mediaDevices && navigator.mediaDevices.getUserMedia
-        if (isVideoSupported) {
-            const getAudioandVideoStream = navigator.mediaDevices.getUserMedia({ audio: true, video: true });
-            getAudioandVideoStream.then(stream => {
-                this.startStreaming(stream);
-                this.enableRecorder(stream);
-            }).catch(error => {
-                console.error('No se pudo acceder a la cámara y/o micrófono.', error);
-            });
-            
-        } else {
-            console.error('La API de getUserMedia no está disponible en este navegador.');
+        if (this.isVideoSupported()) {
+            const videoStreamPromise = navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+            videoStreamPromise.then(this.handleAudioVideoStream)
+                .catch(error => {
+                    console.error('No se pudo acceder a la cámara y/o micrófono.', error);
+                });
         }
     }
 
+    isVideoSupported() {
+        const isVideoSupported = navigator.mediaDevices && navigator.mediaDevices.getUserMedia
+        if (!isVideoSupported) {
+            console.error('La API de getUserMedia no está disponible en este navegador.');
+            return;
+        }
+        return isVideoSupported;
+    }
+    handleAudioVideoStream(stream) {
+        this.startStreaming(stream);
+        this.enableRecorder(stream);
+    }
     disableVideo() {
         if (this.recorder) {
             this.stopStreaming();
@@ -67,13 +73,12 @@ export class VideoRecorder {
         this.recorder.addEventListener('dataavailable', event => {
             this.chunks.push(event.data);
         });
-        this.recorder.addEventListener('stop', () => {
-            this.handleRecordingStop();
-        });
+        this.recorder.addEventListener('stop', this.onRecordingStop);
+
         this.showButton(this.recordBtn);
     }
 
-    handleRecordingStop() {
+    onRecordingStop() {
         const videoData = new Blob(this.chunks, { type: 'video/webm' });
         const videoUrl = URL.createObjectURL(videoData);
         this.videoElement.srcObject = null;
