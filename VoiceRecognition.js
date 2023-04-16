6f20bf10388165ca7be63524e0d49d29884e7125
Andres1~ <andrescorredor20@gmail.com>
2023-04-15 19:10:16 -0500

    clearLabelsAndData

--- VoiceRecognition.js
+++ VoiceRecognition.js
@@ -8,23 +8,47 @@ export class VoiceRecognition {
     startBtn.addEventListener("click", this.startRecognition.bind(this));
 
     this.initChart();
+    this.setupAudioContext();
   }
 
   async initChart() {
     await this.createChartScript();
     this.configChart();
   }
+  async setupAudioContext() {
+    this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    this.analyser = this.audioContext.createAnalyser();
+    this.analyser.fftSize = 1024;
+
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      this.source = this.audioContext.createMediaStreamSource(stream);
+      this.source.connect(this.analyser);
+    } catch (err) {
+      console.error('Error al configurar el AudioContext:', err);
+    }
+  }
+  getVolume() {
+    const dataArray = new Uint8Array(this.analyser.frequencyBinCount);
+    this.analyser.getByteFrequencyData(dataArray);
+
+    const sum = dataArray.reduce((a, b) => a + b, 0);
+    return sum / dataArray.length;
+  }
 
   createHtmlElements() {
     const recordBtn = { tag: 'button', label: 'Iniciar reconocimiento de voz', onClick: null };
     const canvas = { tag: 'canvas', attributes: { id: 'radarChart' } };
     const recordingDiv = { tag: 'div', label: 'Grabando...', style: { display: 'none' } };
+    const progress = { tag: 'progress', attributes: { max: 100, value: 0 } };
     const voiceRecognition = document.querySelector('voice-recognition');
-    const createdElements = HtmlElementsFactory.appendTo(voiceRecognition, [recordBtn, canvas, recordingDiv]);
+    const createdElements = HtmlElementsFactory.appendTo(voiceRecognition, [recordBtn, canvas, recordingDiv, progress]);
     this.recordBtn = createdElements[0];
     this.canvas = createdElements[1];
     this.recordingDiv = createdElements[2];
+    this.progress = createdElements[3];
   }
+  
   createChartScript() {
     return new Promise((resolve) => {
       const script = HtmlElementsFactory.createElement({
@@ -90,7 +114,11 @@ export class VoiceRecognition {
 
   onRecognitionStart() {
     console.log('Micrófono activado. Comenzando a grabar.');
-    this.recordingDiv.style.display = 'block'; // Muestra la etiqueta HTML "div"
+    this.recordingDiv.style.display = 'block';  
+    this.volumeInterval = setInterval(() => {
+      const volume = this.getVolume();
+      this.progress.value = volume;
+    }, 100);
   }
 
 
@@ -103,22 +131,38 @@ export class VoiceRecognition {
     this.recordingDiv.style.display = 'none'; // Oculta la etiqueta HTML "div"
   }
 
-
-
   onRecognitionResult(event) {
     const results = event.results[0];
     console.log(event.results)
-    this.config.data.labels = []; // Vacía las etiquetas antes de agregar nuevas
-    this.config.data.datasets[0].data = []; // Vacía los datos antes de agregar nuevos
+    this.clearLabelsAndData();
+    this.addResultsToChart(results);
+    this.updateChart();
+  }
+
+  clearLabelsAndData() {
+    this.config.data.labels = [];
+    this.config.data.datasets[0].data = [];
+  }
+
+  addResultsToChart(results) {
     for (let i = 0; i < results.length; i++) {
-
       const alternative = results[i];
-      this.config.data.labels.push(alternative.transcript);
-
-      console.log(alternative.transcript)
-      console.log(alternative.confidence)
-      this.config.data.datasets[0].data.push(alternative.confidence);
+      this.addLabel(alternative.transcript);
+      this.addData(alternative.confidence);
     }
+  }
+
+  addLabel(label) {
+    this.config.data.labels.push(label);
+    console.log(label);
+  }
+
+  addData(data) {
+    this.config.data.datasets[0].data.push(data);
+    console.log(data);
+  }
+
+  updateChart() {
     this.radarChart.update();
   }
 }
